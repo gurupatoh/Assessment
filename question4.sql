ebd91651376f2f6c571134e890420430e1fccdca
CodeGuruPatoh <39097684+gurupatoh@users.noreply.github.com>
2020-10-19 14:40:44 +0300

    Update question4.sql

--- question4.sql
+++ question4.sql
@@ -1,6 +1,5 @@
 SELECT institution.name,course.name student.studentid  
 FROM institution,student,course join course 
-ON institution.institutionid = course.institution 
-join student 
+ON institution.institutionid = course.institution join student 
 ON course.courseid = student.course
 
